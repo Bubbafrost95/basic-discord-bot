15bec4c14029ba9cdf8b04a398587e5a6adb37cf
Bubbafrost95 <oabkes@gmail.com>
2017-11-09 17:45:51 -0600

    Dumbass message

--- bot.js
+++ bot.js
@@ -10,13 +10,13 @@ logger.level = 'debug';
 
 //Initialize Discord bot
 var bot = new discord.Client({
-  token: auth.token
+  token: auth.token,
   autorun: true
 });
 bot.on('message', function(user, userID, channelID, message, evt){
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
-  if(message.substring(0,1) == !){
+  if(message.substring(0,1) == '!'){
     var args = message.substring(1).split(' ');
     var cmd = args[0];
 
@@ -25,7 +25,7 @@ bot.on('message', function(user, userID, channelID, message, evt){
       case 'ping':
       bot.sendMessage({
         to: channelID,
-        message: 'Pong! You dumbass';
+        message: 'Pong! You dumbass'
       });
       break;
     }
